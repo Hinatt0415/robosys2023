efbce6821a9c99824d935d85840895989b099b1b
Hinata Sakuma <sakuma@hogehoge.com>
2023-11-29 18:24:19 +0900

    fix test.bash

--- test.bash
+++ test.bash
@@ -17,8 +17,8 @@ out=$(echo あ | ./plus)
 [ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
-out=$(echo | .plus)
-[ "$?" = 1]       || ng ${LINENO}
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
 [ "${out}" = "" ] || ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
